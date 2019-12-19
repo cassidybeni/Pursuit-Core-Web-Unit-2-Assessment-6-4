03f9966e69ddebaf50d3564769cc77a23a9a27c3
cassidybeni <cassidybeni@pursuit.org>
2019-12-18 21:18:49 -0500

    Grab elements

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let div = document.querySelector("div")
+    let form = document.querySelector("form")
+    let review = document.querySelector("#review")
+    let submitReview = document.querySelector("#submitReview")
+    let ul = document.querySelector("ul")
+
+})
